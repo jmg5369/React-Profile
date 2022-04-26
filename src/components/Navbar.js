34756da7f60f645c1cf9173109ebea6c900b7ad1
James Gorodesky <jmg5369@gmail.com>
2022-04-26 17:59:14 -0600

    Fixed errors to complete react deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Navbar(props) {
-   const {currentPage, setCurrentPage} = props; 
+   const {setCurrentPage} = props; 
     return(
         <header>
         <div>
