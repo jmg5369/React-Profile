8ec29320db0b55ac573b32f5209804062fa3081c
James Gorodesky <jmg5369@gmail.com>
2022-06-17 15:15:51 -0600

    added picture to add to profile

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
 return(
     <div>
                     <div className="photo-div">
-            <img className="photo" src="https://scontent.fapa1-2.fna.fbcdn.net/v/t31.18172-8/17310940_3955741060208_721778153056907937_o.jpg?_nc_cat=100&ccb=1-5&_nc_sid=174925&_nc_eui2=AeERB_eo-0dciYgpU5NKLWxA31_CsFGX1YLfX8KwUZfVguNrB83SeqwlqTzwGdmWSYM&_nc_ohc=m77sxupY0ekAX8qiJge&_nc_ht=scontent.fapa1-2.fna&oh=00_AT-B-ZEUy86U0BbsSKqaxJko_UElciX0DVymH5Bb4uyiZQ&oe=626AF9EE" alt="Max Profile Pic"/>
+            <img className="photo" src="https://www.facebook.com/photo/?fbid=3955741060208&set=a.1323392773146" alt="Max Profile Pic"/>
             </div>
             <section className="section-row">
                 <div className="section-heading">
